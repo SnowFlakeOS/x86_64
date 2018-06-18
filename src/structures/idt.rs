e329ba717097c6d14cf913746b6d43facd0fbacc
xuserwhat <quicknshell@gmail.com>
2018-06-18 21:31:20 +0900

    [src/structures/idt.rs] Add 'pub' into interrupts[IdtEntry<HandlerFunc>; 256 - 32]

--- src/structures/idt.rs
+++ src/structures/idt.rs
@@ -361,7 +361,7 @@ pub struct Idt {
     ///   external interrupt was recognized.
     /// - If the interrupt occurs as a result of executing the INTn instruction, the saved
     ///   instruction pointer points to the instruction after the INTn.
-    interrupts: [IdtEntry<HandlerFunc>; 256 - 32],
+    pub interrupts: [IdtEntry<HandlerFunc>; 256 - 32],
 }
 
 impl Idt {
